3132ed0c4696e36411c86411ef910a787236874a
RICHARD NORTH <A0313108@live.tees.ac.uk>
2024-12-04 16:58:28 +0000

    test deployment

--- ThamCoCustomerProductServiceTest/Controllers/companiesControllerTests.cs
+++ ThamCoCustomerProductServiceTest/Controllers/companiesControllerTests.cs
@@ -21,6 +21,7 @@ namespace ThamCoCustomerProductService.Tests.Controllers
         [SetUp]
         public void SetUp()
         {
+
             _mockCompanyService = new Mock<ICompanyService>();
             _mockMapper = new Mock<IMapper>();
             _controller = new CompaniesController(_mockCompanyService.Object, _mockMapper.Object);
